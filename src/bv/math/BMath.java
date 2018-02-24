e25bdeb999650e3726f96a18f17bcb3807079ec7
Brennan Colberg <brennan.colberg@gmail.com>
2018-02-24 13:51:29 -0800

    Moved collisionVelocity(Entity, Entity) to BMath
    self explanatory; updated all references, changed to static method
    - various small changes to Game; cleaning up in preparation of expansion

--- src/bv/math/BMath.java
+++ src/bv/math/BMath.java
@@ -4,6 +4,7 @@
  */
 package bv.math;
 
+import bv.gameFramework.physics.Entity;
 import bv.syntax.BV;
 
 /** 
@@ -14,5 +15,41 @@ public class BMath extends BV {
 	
 	public static double hypot(double...sides) { double sum = 0; for (int i = 0; i < sides.length; i++) sum += Math.pow(sides[i],2); return Math.sqrt(sum); }
 	public static double hypot(CVector cv) { return hypot(cv.getValues()); }
+	
+	public static PVector[] collisionVelocity(Entity object1, Entity object2) {
+		//This is the actual math for calculating velocities of objects after a collision. There are quite a few things wrong with this
+		//One 'problem' being that the collision angle is calculated as if the two objects were circles
+		//In practice this may cause problems or just look weird
+		double m1 = object1.mass;
+		double m2 = object2.mass;
+		double v1 = object1.velocity.getMagnitude();
+		double v2 = object2.velocity.getMagnitude();
+		double theta1 = object1.velocity.getAngle();
+		double theta2 = object2.velocity.getAngle();
+		double phi = object2.position.minus(object1.position).toPVector().getAngle();
+
+		//Rotate the cartesian plane so that it is a 1D collision problem
+		double v1x = v1 * Math.cos(theta1 - phi);
+		double v1y = v1 * Math.sin(theta1 - phi);
+		
+		double v2x = v2 * Math.cos(theta2 - phi);
+		double v2y = v2 * Math.sin(theta2 - phi);
+		
+		//Calculate the new final velocity given the 1D formula:
+		//u1x = ((m1 - m2)v1 + 2m2v2)/(m1 + m2)
+		double u1x = ((m1 - m2) * v1x + 2 * m2 * v2x) / (m1 + m2);
+		double u1y = v1y;
+		CVector u1 = new CVector(u1x, u1y);
+		PVector pu1 = u1.toPVector();
+		pu1.setAngle(pu1.getAngle() + phi);
+		
+		double u2x = ((m2 - m1) * v2x + 2 * m1 * v1x) / (m2 + m1);
+		double u2y = v2y;
+		CVector u2 = new CVector(u2x, u2y);
+		PVector pu2 = u2.toPVector();
+		pu2.setAngle(pu2.getAngle() + phi);
+		
+		return new PVector[] {pu1, pu2};
+	}
 	
 }
--- src/bv/sportsGame/gameStates/Game.java
+++ src/bv/sportsGame/gameStates/Game.java
@@ -13,6 +13,7 @@ import bv.gameFramework.physics.Collidable;
 import bv.gameFramework.physics.Entity;
 import bv.gameFramework.state.GameState;
 import bv.gui.FieldObject;
+import bv.math.BMath;
 import bv.math.CVector;
 import bv.math.PVector;
 import bv.sportsGame.game.entities.Ball;
@@ -49,6 +50,7 @@ public class Game extends GameState {
 	}
 	
 	public void updatePhysics() {
+		
 		// Zooms in when the 1 key is pressed and zooms out when the 2 key is pressed
 		if (Input.isKeyPressed(KeyEvent.VK_1)){
 			if (this.pixelsPerUnit < 1){
@@ -60,9 +62,11 @@ public class Game extends GameState {
 				this.pixelsPerUnit -= 0.01;
 			}
 		}
-		velocity.setAngle(Math.atan2(player.getPosition().getValue(1) - position.getValue(1), player.getPosition().getValue(0) - position.getValue(0)));
 		
-		velocity.setMagnitude(player.getPosition().minus(this.position).toPVector().getMagnitude() / (50 * this.pixelsPerUnit));
+		// creates an exponentially-powered speed; smoothly transitions from moving quickly towards the player when faraway to hardly moving at all when local
+		// always moves at a rate dependent on screen size
+		this.velocity.setAngle(Math.atan2(player.getPosition().getValue(1) - position.getValue(1), player.getPosition().getValue(0) - position.getValue(0)));
+		this.velocity.setMagnitude(player.getPosition().minus(this.position).toPVector().getMagnitude() / (50 * this.pixelsPerUnit));
 		
 		// A very hacky way of deleting all projectiles fired. Press backspace to delete the missiles
 		if (Input.isKeyPressed(KeyEvent.VK_BACK_SPACE)){
@@ -73,67 +77,27 @@ public class Game extends GameState {
 			}
 		}
 		
-		//TODO: For some reason this line of code locks up all key inputs after some time
-//		if (Input.isKeyPressed(KeyEvent.VK_A)){
-//			this.velocity.add(new PVector(-10, player.getVelocity().getAngle()));
-//		}
+		// updates physics for all contained entities
+		super.updatePhysics();
 		
 		checkCollisions();
-		
-		super.updatePhysics();
 	}
 	
 	private void checkCollisions() {
-		//TODO: Collisions between the ball and the player are not being calculated correctly
+		//TODO: Collisions between the ball and the player are not being calculated correctly (??)
 		for (int i = 0; i < objects.size(); i++) if (objects.get(i) instanceof Collidable) {
 			Collidable objectI = (Collidable) objects.get(i);
 			for (int j = i; j < objects.size(); j++) if (objects.get(j) instanceof Collidable) {
 				Collidable objectJ = (Collidable) objects.get(j);
 				if (objectI != objectJ) { 
 					if (objectI.trigger().intersects(objectJ.trigger())) {
-						PVector[] velocities = collisionVelocity((Entity)objectI, (Entity)objectJ);
+						PVector[] velocities = BMath.collisionVelocity((Entity)objectI, (Entity)objectJ);
 						objectI.onCollision(velocities[0], (Entity)objectJ);
 						objectJ.onCollision(velocities[1], (Entity)objectI);
 					}
 				}
 			}
 		}
-	}
-	
-	private PVector[] collisionVelocity(Entity object1, Entity object2) {
-		//This is the actual math for calculating velocities of objects after a collision. There are quite a few things wrong with this
-		//One 'problem' being that the collision angle is calculated as if the two objects were circles
-		//In practice this may cause problems or just look weird
-		double m1 = object1.mass;
-		double m2 = object2.mass;
-		double v1 = object1.velocity.getMagnitude();
-		double v2 = object2.velocity.getMagnitude();
-		double theta1 = object1.velocity.getAngle();
-		double theta2 = object2.velocity.getAngle();
-		double phi = object2.position.minus(object1.position).toPVector().getAngle();
-
-		//Rotate the cartesian plane so that it is a 1D collision problem
-		double v1x = v1 * Math.cos(theta1 - phi);
-		double v1y = v1 * Math.sin(theta1 - phi);
-		
-		double v2x = v2 * Math.cos(theta2 - phi);
-		double v2y = v2 * Math.sin(theta2 - phi);
-		
-		//Calculate the new final velocity given the 1D formula:
-		//u1x = ((m1 - m2)v1 + 2m2v2)/(m1 + m2)
-		double u1x = ((m1 - m2) * v1x + 2 * m2 * v2x) / (m1 + m2);
-		double u1y = v1y;
-		CVector u1 = new CVector(u1x, u1y);
-		PVector pu1 = u1.toPVector();
-		pu1.setAngle(pu1.getAngle() + phi);
-		
-		double u2x = ((m2 - m1) * v2x + 2 * m1 * v1x) / (m2 + m1);
-		double u2y = v2y;
-		CVector u2 = new CVector(u2x, u2y);
-		PVector pu2 = u2.toPVector();
-		pu2.setAngle(pu2.getAngle() + phi);
-		
-		return new PVector[] {pu1, pu2};
 	}
 	
 	public void load() {
