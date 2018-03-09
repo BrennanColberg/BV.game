049bca5ab6edd3392ec9812c687d0bf313791af8
Brennan Colberg <brennan.colberg@gmail.com>
2018-03-08 23:19:14 -0800

    Sprite Overhaul
    Overhauled classes in sprite package
    - did not resolve resulting errors!
    - did not re-add SpriteIO!

--- src/bv/framework/sprites/SpriteFrame.java
+++ src/bv/framework/sprites/SpriteFrame.java
@@ -5,28 +5,27 @@ import java.util.ArrayList;
 
 import bv.framework.graphics.Renderer;
 import bv.framework.math.CVector;
-import bv.framework.state.Tickable;
+import bv.framework.math.Poly;
 
 @SuppressWarnings("serial")
-public class Sprite extends ArrayList<SpriteFrame> implements Tickable {
+public class SpriteFrame extends ArrayList<Poly> {
 
-	public int index = 0;
 	public double scale = 1;
 	public double heading = 0;
 	public Color color = Color.MAGENTA;
 	
-	public Sprite(SpriteFrame...frames) {
-		for (SpriteFrame sf:frames) {
-			this.add(sf);
+	public SpriteFrame(Poly...polies) {
+		for (Poly p:polies) {
+			this.add(p);
 		}
 	}
 	
-	public void tick() {
-		index++;
-	}
-	
 	public void render(Renderer r, CVector position, double scale, double heading, Color color) {
-		this.get(index % this.size()).render(r, position, scale, heading, color);
+		for (int i = 0; i < this.size(); i++) {
+			Poly poly = this.get(i).rotatedBy(heading).scaledBy(scale);
+			poly.setPosition(position);
+			r.fill(poly, color);
+		}
 	}
 	public void render(Renderer r, CVector position, double scale, double heading) {
 		this.render(r, position, scale, heading, this.color);
@@ -37,5 +36,5 @@ public class Sprite extends ArrayList<SpriteFrame> implements Tickable {
 	public void render(Renderer r, CVector position) {
 		this.render(r, position, this.scale, this.heading, this.color);
 	}
-
+	
 }
