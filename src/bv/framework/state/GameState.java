3fcda37e9789802e49ae6967d4d56273618a3c23
Brennan Colberg <brennan.colberg@gmail.com>
2018-03-10 13:09:02 -0800

    Documented GameState & fixed call hierarchy issues
    - Documented GameState
    - separated updatePhysics, render, and tick in GameState from the larger call methods that propogate through GameState.objects
    - updated methods in GameStateManager to reflect this change

--- src/bv/framework/state/GameState.java
+++ src/bv/framework/state/GameState.java
@@ -27,52 +27,69 @@ public abstract class GameState extends Entity implements Renderable, Tickable {
 	
 	/* VARIABLES */
 	
+	/** Used as a scaling factor; increase to zoom out, decrease to zoom in. */
 	public double pixelsPerUnit = 1;
+	
+	/** This {@link ArrayList} of {@link Object}s is used to store ALL relevant objects to a single GameState.
+	 * Any object should be placed here, and will automatically be dealt with:
+	 *  {@link Tickable}s will be ticked,
+	 *  {@link Renderable}s will be rendered,
+	 *  and {@link Physics} objects will be updatePhysicsed. */
 	public ArrayList<Object> objects = new ArrayList<Object>();
+
 	public CVector maxBounds = new CVector(Core.STARTING_SCREEN_SIZE.getValue(0) * 2, Core.STARTING_SCREEN_SIZE.getValue(1) * 2);
 	public CVector minBounds = new CVector(-Core.STARTING_SCREEN_SIZE.getValue(0) * 2, -Core.STARTING_SCREEN_SIZE.getValue(1) * 2);
+	
+	
 	/* CONSTRUCTORS */
 	
+	/** Default constructor; simply sets magnification to normal (1 PPU) and calls init(). DO NOT OVERRIDE; instead, put custom code into init() */
 	public GameState() {
-		pixelsPerUnit = 1;
-		init();
+		this(1);
 	}
 	
-	@Deprecated
+	/** Custom zoom constructor; sets magnification to what you would like (see {@link GameState.pixelsPerUnit}. DO NOT OVERRIDE; instead, put custom code into init()
+	 * @param pixelsPerUnit is the custom zoom you would like. */
 	public GameState(double pixelsPerUnit) {
 		this.pixelsPerUnit = pixelsPerUnit;
 		init();
 	}
 	
-	/** Runs when this {@link GameState} is first created; use this rather than overriding the constructors
-	 * 
-	 */
+	/** Runs when this {@link GameState} is first created; use this for custom code rather than overriding any constructors. 
+	 * Completely empty by default; blank canvas for your enjoyment! */
 	public abstract void init();
 
 	
 	/* METHODS */
 	
-	public void tick() {
+	/** Passes on the tick() command to ALL {@link Tickable} objects in its objects list. Automatic. */
+	public final void tickObjects() {
+		this.tick();
 		for (int i = 0; i < objects.size(); i++) {
 			if (objects.get(i) instanceof Tickable) ((Tickable) objects.get(i)).tick();
 		}
 	}
 	
-	public void updatePhysics() {
-		super.updatePhysics();
+	/** Passes on the updatePhysics() command to ALL {@link Physics}-implementing objects in its objects list. Automatic. */
+	public final void updateObjectPhysics() {
+		this.updatePhysics();
 		for (int i = 0; i < objects.size(); i++) {
 			if (objects.get(i) instanceof Physics) ((Physics) objects.get(i)).updatePhysics();
 		}
 	}
 
-	public void render(Renderer r) {
+	/** Passes on the render(r) command to ALL {@link Renderable} objects in its objects list. Automatic. */
+	public final void renderObjects(Renderer r) {
+		this.render(r);
 		for (int i = 0; i < objects.size(); i++) {
 			if (objects.get(i) instanceof Renderable) ((Renderable) objects.get(i)).render(r);
-
 		}
 	}
+	public void render(Renderer r) { }
 	
-	public void calculateCollisions() {
+	/** Fun math stuff to calculate collisions between two {@link Collidable} objects!
+	 * Automatically considers all {@link Collidable} objects, and if any are overlapping calls each's onCollision method. */
+	public final void calculateCollisions() {
 		for (int i = 0; i < objects.size(); i++) if (objects.get(i) instanceof Collidable) {
 			Collidable objectI = (Collidable) objects.get(i);
 			for (int j = i; j < objects.size(); j++) if (objects.get(j) instanceof Collidable) {
@@ -88,21 +105,22 @@ public abstract class GameState extends Entity implements Renderable, Tickable {
 		}
 	}
 	
-	/** Runs every time this GameState is newly called into the {@link GameStateManager}'s focus.
-	 * 
-	 * @return nada
-	 * @author	Brennan Colberg
-	 * @since	Jan 17, 2018
-	 */
+	/** Runs every time this GameState is newly called into the {@link GameStateManager}'s focus. 
+	 * Completely empty, blank canvas for your enjoyment. */
 	public abstract void load();
+	
 	
 	/* GETTERS & SETTERS */
 	
+	/** @return the size of the currently rendered screen, in {@link CVector} form */
 	public CVector getSize() {
 		return (CVector) Core.renderEngine.getDisplay().getSize().scaledBy(1/pixelsPerUnit);
 	}
+	
+	
 	/* TECHNICAL METHODS */
 	
+	/** @return the currently rendered field, accurate in size and position, in {@link Rect} form */
 	public Rect rectBounds() {
 		Rect result = Core.renderEngine.getDisplay().rectBounds();
 		result.setPosition(getPosition());
@@ -110,14 +128,18 @@ public abstract class GameState extends Entity implements Renderable, Tickable {
 		return result;
 	}
 	
+	/** @return the currently rendered field, accurate in size and position, in {@link Poly} form of a {@link Rect} */
 	public Poly polyBounds() {
 		return rectBounds().polyBounds();
 	}	
-	public boolean inBounds(CVector position) {
+	
+	/** @param point the point to test, in {@link CVector} form
+	 * 	@return whether or not a given point (in {@link CVector} form) is within the currently rendered screen */
+	public boolean inBounds(CVector point) {
 		
-		return ((position.getValue(0) > minBounds.getValue(0)) &&
-				(position.getValue(1) > minBounds.getValue(1)) &&
-				(position.getValue(0) < maxBounds.getValue(0)) &&
-				(position.getValue(1) < maxBounds.getValue(1)));
+		return ((point.getValue(0) > minBounds.getValue(0)) &&
+				(point.getValue(1) > minBounds.getValue(1)) &&
+				(point.getValue(0) < maxBounds.getValue(0)) &&
+				(point.getValue(1) < maxBounds.getValue(1)));
 	}
 }
