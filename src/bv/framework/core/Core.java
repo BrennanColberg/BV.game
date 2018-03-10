dfbaaec2fcb012758d99451fe7e19fb275cbe505
Brennan Colberg <brennan.colberg@gmail.com>
2018-03-10 15:44:37 -0800

    Documentation Written: bv.framework.core
    bv.framework.core.Core
    bv.framework.core.Display
    bv.framework.core.GameStateManager
    bv.framework.core.Input

--- src/bv/framework/core/Core.java
+++ src/bv/framework/core/Core.java
@@ -1,7 +1,3 @@
-/** 
- * @author	Brennan Colberg
- * @since	Dec 7, 2017
- */
 package bv.framework.core;
 
 import bv.framework.core.engines.CollisionEngine;
@@ -11,36 +7,54 @@ import bv.framework.math.CVector;
 import bv.framework.sprites.SpriteIO;
 import bv.framework.state.GameState;
 
-/** 
- * @author	Brennan Colberg
- * @since	Dec 7, 2017
- */
+/** Centrally static class around which the running program revolves.
+ *  You can see it as the hub; all managers and threads can be accessed through a static call to this class.
+ *  Contains the main(String[]) method. 
+ *  Largely just used to coordinate startup and initialization of all necessary framework components without a spaghettified hassle...
+ * @author Brennan D. Colberg - {@link http://github.com/BrennanColberg} */
 public class Core {
 	
+	/** The {@link CVector} that dictates at what size the screen should start. */
 	public static final CVector STARTING_SCREEN_SIZE = new CVector(1024,576);
 	
+	/** Decrees whether or not updates such as TPS and FPS should show in console. */
 	public static boolean printEngineUpdatesToConsole = true;
+	// no idea what this does
 	public static int engineUpdateDelay = 1000;
+	// good lord this is spaghetti, are these even used?
 	public static boolean running = true, ticking = true, rendering = true;
 	
+	/** The {@link GameStateManager} that the running program uses. */
 	public static GameStateManager gameStateManager;
+	/** The {@link TickEngine} that the running program uses. */
 	public static TickEngine tickEngine;
+	/** The {@link RenderEngine} that the running program uses. */
 	public static RenderEngine renderEngine;
+	/** The {@link CollisionEngine} that the running program uses. */
 	public static CollisionEngine collisionEngine;
+	
+	/** The method used to start up the entire program! 
+	 * 	@param args console inputs, unused in our case */
 	public static void main(String[] args) {
 		
+		// loads all Sprites and AnimatedSprites into an organized database from the src/sprites folder
 		SpriteIO.load();
 		
+		// initializes various engines and managers
 		renderEngine = new RenderEngine(90, STARTING_SCREEN_SIZE);
 		gameStateManager = new GameStateManager();
 		tickEngine = new TickEngine(90);
 		collisionEngine = new CollisionEngine(30);
+		
+		// starts all three threads; program officially begins here!
 		renderEngine.start();
 		tickEngine.start();
 		collisionEngine.start();
 		
 	}
 	
+	/** A shorthand method; this simplifies "Core.gameStateManager.currentState.[probably two more things]" into "Core.state().[other stuff]" to make life easier.
+	 * @return the program's currently focused {@link GameState} */
 	public static GameState state() {
 		return gameStateManager.currentState;
 	}
--- src/bv/framework/core/Input.java
+++ src/bv/framework/core/Input.java
@@ -1,7 +1,3 @@
-/** 
- * @author	Brennan Colberg
- * @since	Dec 17, 2017
- */
 package bv.framework.core;
 
 import java.awt.Point;
@@ -15,55 +11,147 @@ import java.awt.event.MouseWheelListener;
 import java.util.HashMap;
 
 import bv.framework.math.CVector;
+import bv.framework.state.GameState;
 
-/** 
- * @author	Brennan Colberg
- * @since	Dec 17, 2017
- */
+/** A static class used to collect and categorize all input (from both the keyboard and mouse).
+ * @author Brennan D. Colberg - {@link http://github.com/BrennanColberg} */
 public class Input implements KeyListener, MouseListener, MouseMotionListener, MouseWheelListener {
 	
+	/* VARIABLES */
+	
 	public static Display display = null;
-	public static boolean live = false;
 	
-	protected static Long lastMousePress = 0L;
-	protected static Long lastMouseRelease = 0L;
-	protected static Long lastRightMousePress = 0L;
-	protected static Long lastRightMouseRelease = 0L;
-	protected static CVector lastMousePressPosition = new CVector(0,0);
-	protected static CVector lastMousePressAdjustedPosition = new CVector(0,0);
+	// click time
+	
+	/** The last time the left mouse button was pressed, in milliseconds, in {@link Long} form. */
+	protected static Long lastLeftClickPress = 0L;
+	/** The last time the right mouse button was pressed, in milliseconds, in {@link Long} form. */
+	protected static Long lastRightClickPress = 0L;
+	/** The last time the left mouse button was released, in milliseconds, in {@link Long} form. */
+	protected static Long lastLeftClickRelease = 0L;
+	/** The last time the right mouse button was released, in milliseconds, in {@link Long} form. */
+	protected static Long lastRightClickRelease = 0L;
+	
+	// click position
+	
+	/** The last position at which the left mouse button was pressed, relative to the {@link Display}'s top left corner, in {@link CVector} form. */
+	protected static CVector lastClickPosition = new CVector(0,0);
+	/** The last position at which the right mouse button was pressed, relative to the {@link Display}'s top left corner, in {@link CVector} form. */
+	protected static CVector lastRightClickPosition = new CVector(0,0);
+	/** The last position at which the left mouse button was pressed, relative to the {@link Display}'s current center and scaled appropriately to the current {@link GameState}, in {@link CVector} form. */
+	protected static CVector lastClickAdjustedPosition = new CVector(0,0);
+	/** The last position at which the right mouse button was pressed, relative to the {@link Display}'s current center and scaled appropriately to the current {@link GameState}, in {@link CVector} form. */
+	protected static CVector lastRightClickAdjustedPosition = new CVector(0,0);
+	
+	// mouse position
+	
+	/** The current position of the mouse pointer, relative to the {@link Display}'s top left corner, in {@link CVector} form. */
 	protected static CVector mousePosition = new CVector(0,0);
+	/** The current position of the mouse pointer, relative to the {@link Display}'s current center and scaled appropriately to the current {@link GameState}, in {@link CVector} form. */
 	protected static CVector mouseAdjustedPosition = new CVector(0,0);
-	protected static boolean recentMousePress = false;
+
+	// key hashmap data
 	
-	public static Long getLastMousePress() { return lastMousePress; }
-	public static Long getLastMouseRelease() { return lastMouseRelease; }
-	public static boolean isMousePressed() { return lastMouseRelease < lastMousePress; }
-	public static boolean isRightMousePressed() { return lastRightMouseRelease < lastRightMousePress; }
-	public static boolean wasMousePressedRecently() { return recentMousePress; }
+	/** A {@link HashMap} that stores the time, in milliseconds, each key was last pressed (in {@link Long} form, accessed by {@link KeyEvent}.VK_[KEY] {@link Integer}s). */
+	protected static HashMap<Integer,Long> lastKeyPress = new HashMap<Integer,Long>();
+	/** A {@link HashMap} that stores the time, in milliseconds, each key was last released (in {@link Long} form, accessed by {@link KeyEvent}.VK_[KEY] {@link Integer}s). */
+	protected static HashMap<Integer,Long> lastKeyRelease = new HashMap<Integer,Long>();
 	
-	public static CVector getLastMousePressPosition() { return lastMousePressPosition; }
-	public static CVector getLastMousePressAdjustedPosition() { return lastMousePressAdjustedPosition; }
+	
+	/* GETTERS */
+	
+	// click time
+	
+	/** @return the last time the left mouse button was pressed, in milliseconds, in {@link Long} form */
+	public static Long getLastClickTime() { return lastLeftClickPress; }
+	/** @return the last time the right mouse button was pressed, in milliseconds, in {@link Long} form */
+	public static Long getLastRightClickTime() { return lastRightClickPress; }
+	/** @return whether or not the left mouse button is currently pressed, in {@link Boolean} form */
+	public static boolean isMousePressed() { return lastLeftClickRelease < lastLeftClickPress; }
+	/** @return whether or not the right mouse button is currently pressed, in {@link Boolean} form */
+	public static boolean isRightMousePressed() { return lastRightClickRelease < lastRightClickPress; }
+//	/** @return whether or not the left mouse button was recently pressed, in {@link Boolean} form. Returns true for exactly one tick per press. */
+//	public static boolean recentClick() { return recentLeftClick; }
+//	/** @return whether or not the right mouse button was recently pressed, in {@link Boolean} form. Returns true for exactly one tick per press. */
+//	public static boolean recentRightClick() { return recentRightClick; }
+	
+	// click position
+	
+	/** @return the last position at which the left mouse button was pressed, relative to the {@link Display}'s top left corner, in {@link CVector} form. */
+	public static CVector getLastClickPosition() { return lastClickPosition; }
+	/** @return the last position at which the right mouse button was pressed, relative to the {@link Display}'s top left corner, in {@link CVector} form. */
+	public static CVector getLastRightClickPosition() { return lastRightClickPosition; }
+	/** @return the last position at which the left mouse button was pressed, relative to the {@link Display}'s current center and scaled appropriately to the current {@link GameState}, in {@link CVector} form. */
+	public static CVector getLastClickAdjustedPosition() { return lastClickAdjustedPosition; }
+	/** @return the last position at which the right mouse button was pressed, relative to the {@link Display}'s current center and scaled appropriately to the current {@link GameState}, in {@link CVector} form. */
+	public static CVector getLastRightClickAdjustedPosition() { return lastRightClickAdjustedPosition; }
+	
+	// mouse position
+	
+	/** @return the current position of the mouse pointer, relative to the {@link Display}'s top left corner, in {@link CVector} form. */
 	public static CVector getMousePosition() { return mousePosition; }
+	/** @return the current position of the mouse pointer, relative to the {@link Display}'s current center and scaled appropriately to the current {@link GameState}, in {@link CVector} form. */
 	public static CVector getMouseAdjustedPosition() { return mouseAdjustedPosition; }
 	
-	protected static HashMap<Integer,Long> lastKeyPress = new HashMap<Integer,Long>();
-	protected static HashMap<Integer,Long> lastKeyRelease = new HashMap<Integer,Long>();
-	protected static HashMap<Integer,Boolean> recentKeyPress = new HashMap<Integer,Boolean>();
+	// key data
 	
-	public static Long getLastKeyPress(int index) { if (lastKeyPress.containsKey(index)) return lastKeyPress.get(index); else { lastKeyPress.put(index, 0L); return 0L; } }
-	public static Long getLastKeyRelease(int index) { if (lastKeyRelease.containsKey(index)) return lastKeyRelease.get(index); else { lastKeyRelease.put(index, 0L); return 0L; } }
+	/** @param index the relevant key, in {@link Integer} form (accessible through {@link KeyEvent}.VK_[KEY])
+	 * @return the time, in milliseconds, the key was last pressed (in {@link Long} form) */
+	public static Long getLastKeyPress(int index) { 
+		if (lastKeyPress.containsKey(index)) return lastKeyPress.get(index); 
+		else { lastKeyPress.put(index, 0L); return 0L; } 
+	}
+	/** @param index the relevant key, in {@link Integer} form (accessible through {@link KeyEvent}.VK_[KEY])
+	 * @return the time, in milliseconds, the key was last released (in {@link Long} form) */
+	public static Long getLastKeyRelease(int index) {
+		if (lastKeyRelease.containsKey(index)) return lastKeyRelease.get(index); 
+		else { lastKeyRelease.put(index, 0L); return 0L; }
+	}
+	/** @param index the relevant key, in {@link Integer} form (accessible through {@link KeyEvent}.VK_[KEY])
+	 * @return whether or not the key is currently pressed (in {@link Boolean} form) */
 	public static boolean isKeyPressed(int index) { return getLastKeyRelease(index) < getLastKeyPress(index); }
-	public static boolean wasKeyPressedRecently(int index) { if (recentKeyPress.containsKey(index)) return recentKeyPress.get(index); else { recentKeyPress.put(index, isKeyPressed(index)); return isKeyPressed(index); } }
 	
-	@Deprecated public void mouseWheelMoved(MouseWheelEvent e) {}
-	@Deprecated public void mouseMoved(MouseEvent e) {}
-	@Deprecated public void mousePressed(MouseEvent e) { if(e.getButton() == MouseEvent.BUTTON3) { lastRightMousePress = e.getWhen(); } else { lastMousePress = e.getWhen(); }; lastMousePressPosition = new CVector(e.getX(), e.getY()); lastMousePressAdjustedPosition = Core.renderEngine.renderer.normalize(lastMousePressPosition); }
-	@Deprecated public void mouseReleased(MouseEvent e) { lastMouseRelease = e.getWhen(); lastRightMouseRelease = e.getWhen(); }
-	@Deprecated public void keyTyped(KeyEvent e) {}
+
+	/** Deprecated, automatic method. Used to update mouse click time and position for both right and left clicks.
+	 * @param e	Automatically generated and inputted MouseEvent; describes the click and is used to gather data */
+	@Deprecated public void mousePressed(MouseEvent e) { 
+		switch (e.getButton()) {
+			case MouseEvent.BUTTON1:
+				lastLeftClickPress = e.getWhen();
+				lastClickPosition = new CVector(e.getX(), e.getY()); 
+				lastClickAdjustedPosition = Core.renderEngine.renderer.normalize(lastClickPosition); 
+				break;
+			case MouseEvent.BUTTON2:
+				lastRightClickPress = e.getWhen();
+				lastRightClickPosition = new CVector(e.getX(), e.getY()); 
+				lastRightClickAdjustedPosition = Core.renderEngine.renderer.normalize(lastRightClickPosition); 
+				break;
+			default: break;
+		}
+	}
+	/** Deprecated, automatic method. Used to update mouse button release time and position for both right and left clicks.
+	 * @param e	Automatically generated and inputted MouseEvent; describes the release and is used to gather data */
+	@Deprecated public void mouseReleased(MouseEvent e) { 
+		switch (e.getButton()) {
+			case MouseEvent.BUTTON1:
+				lastLeftClickRelease = e.getWhen();
+				break;
+			case MouseEvent.BUTTON2: 
+				lastRightClickRelease = e.getWhen(); 
+				break;
+			default: break;
+		}
+	}
+
+	/** Deprecated, automatic method. Used to update key press time.
+	 * @param e	Automatically generated and inputted KeyEvent; describes the press and is used to gather data */
 	@Deprecated public void keyPressed(KeyEvent e) { lastKeyPress.put(e.getKeyCode(), e.getWhen()); }
+	/** Deprecated, automatic method. Used to update key release time.
+	 * @param e	Automatically generated and inputted KeyEvent; describes the release and is used to gather data */
 	@Deprecated public void keyReleased(KeyEvent e) { lastKeyRelease.put(e.getKeyCode(), e.getWhen()); }
 	
-	public static void tick() {
+	/** Deprecated, automatic method. Used to actively update mouse position at a quicker refresh rate then the passive mouseMoved(MouseEvent). */
+	@Deprecated public static void tick() {
 		Point mousePoint = null;
 		if (display != null) mousePoint = display.getFrame().getMousePosition();
 		if (mousePoint != null) {
@@ -72,26 +160,13 @@ public class Input implements KeyListener, MouseListener, MouseMotionListener, M
 		}
 	}
 	
-	
-	@Override
-	public void mouseDragged(MouseEvent e) {
-		// TODO Auto-generated method stub
-		
-	}
-	@Override
-	public void mouseClicked(MouseEvent e) {
-		// TODO Auto-generated method stub
-		
-	}
-	@Override
-	public void mouseEntered(MouseEvent e) {
-		// TODO Auto-generated method stub
-		
-	}
-	@Override
-	public void mouseExited(MouseEvent e) {
-		// TODO Auto-generated method stub
-		
-	}
+	// unused but necessary implements
+	public void mouseWheelMoved(MouseWheelEvent e) {}
+	public void mouseMoved(MouseEvent e) {}
+	public void keyTyped(KeyEvent e) {}
+	public void mouseDragged(MouseEvent e) {}
+	public void mouseClicked(MouseEvent e) {}
+	public void mouseEntered(MouseEvent e) {}
+	public void mouseExited(MouseEvent e) {}
 	
 }
